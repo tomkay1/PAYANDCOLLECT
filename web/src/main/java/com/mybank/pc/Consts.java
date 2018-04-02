52c151e963dc4d965f7310724cf3f8e601144f8a
于海慧 <125227112@qq.com>
2018-04-02 09:39:02 +0800

    优化常量类的类型

--- web/src/main/java/com/mybank/pc/Consts.java
+++ web/src/main/java/com/mybank/pc/Consts.java
@@ -8,35 +8,35 @@ import java.math.BigDecimal;
  * Created by yuhaihui8913 on 2017/11/14.
  * 常量类
  */
-public class Consts {
+public interface Consts {
 
-    public static final Whitelist basicWithImages=Whitelist.basicWithImages();
+    Whitelist basicWithImages = Whitelist.basicWithImages();
 
-    public static final String ENCRYPT_KEY="mbpc-never-late";
+    String ENCRYPT_KEY = "mbpc-never-late";
 
-    public static final String USER_ACCESS_TOKEN="userId-mbpc";
+    String USER_ACCESS_TOKEN = "userId-mbpc";
 
-    public static final int COOKIE_TIMEOUT=4*60*60;
+    int COOKIE_TIMEOUT = 4 * 60 * 60;
 
-    public static final int COOKIE_FOREVER=24*60*60*6*365*50;
+    int COOKIE_FOREVER = 24 * 60 * 60 * 6 * 365 * 50;
 
-    public static final String CURR_USER="currUser";
+    String CURR_USER = "currUser";
 
-    public static final String CURR_USER_MER="currUserMer";
+    String CURR_USER_MER = "currUserMer";
 
-    public static final String CURR_USER_ROLES="currUserRoles";
+    String CURR_USER_ROLES = "currUserRoles";
 
-    public static final String CURR_USER_RESES="currUserReses";
+    String CURR_USER_RESES = "currUserReses";
 
-    public static final String T_CATALOG_CK="catalog";
+    String T_CATALOG_CK = "catalog";
 
-    public static final String T_TAG_CK="tag";
+    String T_TAG_CK = "tag";
 
-    public static final String BLANK="";
+    String BLANK = "";
 
-    public static final BigDecimal ZERO=new BigDecimal(0);
+    BigDecimal ZERO = new BigDecimal(0);
 
-    public enum YORN {
+    enum YORN {
         yes(true), no(false);
         boolean val;
 
@@ -53,7 +53,7 @@ public class Consts {
         }
     }
 
-    public enum YORN_STR {
+    enum YORN_STR {
         yes("0"), no("1");
         String val;
 
@@ -73,53 +73,57 @@ public class Consts {
     /**
      * @param
      * @author: 于海慧  2016/12/10
-     * @Description:  状态枚举
+     * @Description: 状态枚举
      * @return void
      * @throws
      **/
-    public enum STATUS {
+    enum STATUS {
         enable("0"), forbidden("1");
         String val;
 
         private STATUS(String val) {
             this.val = val;
         }
-        public String getVal(){
+
+        public String getVal() {
             return this.val;
         }
-        public String getValTxt(){
-            return (val.equals("0")?"正常":"禁用");
+
+        public String getValTxt() {
+            return (val.equals("0") ? "正常" : "禁用");
         }
     }
 
-    public enum REQ_JSON_CODE {
-        success, fail,unauthorized;
+    enum REQ_JSON_CODE {
+        success, fail, unauthorized;
     }
 
-    public enum CHECK_STATUS{
-        normal("00"), waitingCheck("01"),revokeCheck("02");
+    enum CHECK_STATUS {
+        normal("00"), waitingCheck("01"), revokeCheck("02");
         String val;
 
         private CHECK_STATUS(String val) {
             this.val = val;
         }
-        public String getVal(){
+
+        public String getVal() {
             return this.val;
         }
-        public String getValTxt(){
-            if(val.equals("00")){
+
+        public String getValTxt() {
+            if (val.equals("00")) {
                 return "正常";
-            }else if(val.equals("01")){
+            } else if (val.equals("01")) {
                 return "等待审批";
-            }else if(val.equals("02")){
+            } else if (val.equals("02")) {
                 return "未通过审批";
             }
             return "";
         }
     }
 
-    public enum CACHE_NAMES {
-        paramCache,ssq,userRoles,user,userReses,taxonomy,art,carouselsetting,login
+    enum CACHE_NAMES {
+        paramCache, ssq, userRoles, user, userReses, taxonomy, art, carouselsetting, login
     }
 
 }
