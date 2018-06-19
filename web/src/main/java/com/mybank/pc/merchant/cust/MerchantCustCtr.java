0f62f3745a41abb5593665cf2cd8fc0ca6bb8747
xufei0718 <xufei0718@163.com>
2018-06-19 17:03:21 +0800

    修改绑定银行卡成功策略

--- web/src/main/java/com/mybank/pc/merchant/cust/MerchantCustCtr.java
+++ web/src/main/java/com/mybank/pc/merchant/cust/MerchantCustCtr.java
@@ -107,22 +107,26 @@ public class MerchantCustCtr extends CoreController {
         kv.set("expired", "");
         Kv[] resKv = cEntrustSrv.establishAll(kv);
 
-        for (int i = 0; i < resKv.length; i++) {
-            if (!resKv[i].getBoolean("isSuccess")) {
-                String errMsg;
-                if (ObjectUtil.isNotNull(resKv[i].get("unionpayEntrust"))) {
-                    errMsg = ((UnionpayEntrust) resKv[i].get("unionpayEntrust")).getRespMsg();
-                    if (ObjectUtil.isNull(errMsg)) {
-                        errMsg = "远程调用失败（系统内部异常）！";
-                    }
-                } else {
-                    errMsg = "远程调用异常！";
+        boolean isSucc1 =resKv[0].getBoolean("isSuccess");
+        boolean isSucc2 =resKv[1].getBoolean("isSuccess");
+        boolean isSucc3 =resKv[2].getBoolean("isSuccess");
+        //只有三总情况都失败时，判断绑定失败，默认显示最后失败信息
+        if (!isSucc1 && !isSucc2 && !isSucc3) {
+            String errMsg;
+            if (ObjectUtil.isNotNull(resKv[2].get("unionpayEntrust"))) {
+                errMsg = ((UnionpayEntrust) resKv[2].get("unionpayEntrust")).getRespMsg();
+                if (ObjectUtil.isNull(errMsg)) {
+                    errMsg = "远程调用失败（系统内部异常）！";
                 }
-
-                renderFailJSON(errMsg);
-                return;
+            } else {
+                errMsg = "远程调用异常！";
             }
+
+            renderFailJSON(errMsg);
+            return;
         }
+
+
 
         merCust.save();
         renderSuccessJSON("新增客户信息成功。", "");
